c1bedecee93e1002a518c7a715d21d0a6926cc75
mahmudul11223344 <islamashraf128@gmail.com>
2025-03-12 15:44:56 +0600

    3rd commit

--- script/index.js
+++ script/index.js
@@ -2,11 +2,32 @@ function loadCategories(){
     // fetch the data
 
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    // convert promise to json
     .then((res)=>res.json())
+    // send data to display 
     .then((data)=>displayCategories(data.categories));
 }
 
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+
 function displayCategories(categories){
-console.log(categories);
+// get the container
+    const categoryContainer = document.getElementById("category-container");
+
+    // loop operation on Array of object
+    for( let cat of categories){
+        console.log(cat)
+
+        // create element 
+    const categoryDiv = document.createElement("div");
+    categoryDiv.innerHTML=`
+    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+    `
+    // Append the Element
+    categoryContainer.appendChild(categoryDiv);
+    }
 }
 loadCategories();
