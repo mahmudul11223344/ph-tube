0fe25cb3c3d1211995802dc405fbee931bd7b502
mahmudul11223344 <islamashraf128@gmail.com>
2025-03-12 21:11:53 +0600

    4rth commit

--- script/index.js
+++ script/index.js
@@ -1,33 +1,66 @@
-function loadCategories(){
+function loadCategories() {
     // fetch the data
 
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    // convert promise to json
-    .then((res)=>res.json())
-    // send data to display 
-    .then((data)=>displayCategories(data.categories));
+        // convert promise to json
+        .then((res) => res.json())
+        // send data to display 
+        .then((data) => displayCategories(data.categories));
 }
 
-// {
-//     "category_id": "1001",
-//     "category": "Music"
-// }
 
-function displayCategories(categories){
-// get the container
+
+function loadVideos() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((res => res.json()))
+        .then((data) => displayVideos(data.videos));
+}
+
+function displayCategories(categories) {
+    // get the container
     const categoryContainer = document.getElementById("category-container");
 
     // loop operation on Array of object
-    for( let cat of categories){
-        console.log(cat)
+    for (let cat of categories) {
+        // console.log(cat)
 
         // create element 
-    const categoryDiv = document.createElement("div");
-    categoryDiv.innerHTML=`
+        const categoryDiv = document.createElement("div");
+        categoryDiv.innerHTML = `
     <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
     `
-    // Append the Element
-    categoryContainer.appendChild(categoryDiv);
+        // Append the Element
+        categoryContainer.appendChild(categoryDiv);
     }
 }
+
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById("video-container");
+
+    videos.forEach(video => {
+        console.log(video);
+
+        const videoCard = document.createElement("div");
+        videoCard.innerHTML = `
+        <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        `
+        // append
+        videoContainer.append(videoCard)
+    })
+}
+
 loadCategories();
+loadVideos();
